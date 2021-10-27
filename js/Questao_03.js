9bb7e8d21f6cf4e2d61e006ade69bbfa1c626d8e
dougovski <88936801+dougovski@users.noreply.github.com>
2021-10-27 00:17:24 +0000

    Initial commit

---
+++ js/Questao_03.js
@@ -0,0 +1,13 @@
+var nomeAluno = "Douglas Henrique";
+var nota1 = 10;
+var nota2 = 9.4;
+var nota1_peso = nota1 * 0.6;
+var nota2_peso = nota2 * 0.4;
+var notafinal = nota1_peso + nota2_peso;
+
+alert("Nome do aluno: " + nomeAluno + "\n" + 
+	  "Nota 1: " + nota1 + "\n" + 
+	  "Nota 2: " + nota2 + "\n");
+
+alert('Nome do aluno: ' + nomeAluno + "\n" + 
+	  "Nota final: " + notafinal);
---
+++ js/Questao_04.js
@@ -0,0 +1,35 @@
+class Aluno {
+  constructor(nome, sobrenome, nota1, nota2) {
+    this.primeiroNome = nome;
+    this.segundoNome = sobrenome;
+    this.primeiraNota = nota1;
+    this.segundaNota = nota2;     
+  }
+  nomeCompleto() {
+	  return (this.primeiroNome + " " + this.segundoNome);
+  }
+  media(){
+	  return (this.primeiraNota * 0.6) + (this.segundaNota * 0.4);	  
+  }
+  situacao(){
+	  if (this.media() > 6){
+		  return 'Aprovado';
+	  }
+	  return 'Reprovado';
+  }
+}
+
+function criarAlunos(){
+	var alunos = [new Aluno('Douglas', 'Henrique', 1.0, 4.5),
+		          new Aluno('Gustavo', 'Scarpa', 9.0, 7.5)];	
+	
+	alunos.forEach(mostrarAlunos);
+}
+
+function mostrarAlunos(item, index) {
+	  alert("Nome completo: " + item.nomeCompleto() + "\n" + 
+			"Média: " + item.media() + "\n" + 
+			"Situação: " + item.situacao());
+}
+
+criarAlunos();
